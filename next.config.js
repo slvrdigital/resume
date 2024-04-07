7a6ef13f818650f1c7a072e7337aecd0ae0f4120
Vitalia <vita.kshevitska@gmail.com>
2024-04-07 17:24:16 +0200

    chore: add csp config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+`;
+
 const nextConfig = {
   async headers() {
     return [
@@ -9,6 +23,15 @@ const nextConfig = {
           {
             key: "Cache-Control",
             value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
           },
         ],
       },
