811a6d01579ccfcbcbdab6981519c007fa515cf9
Vitalia <vita.kshevitska@gmail.com>
2024-04-07 17:19:20 +0200

    chore: add cache-control header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|woff2)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
