f1bd27b52be68fd7b67aa1dcb99ff96a88bf17f9
Vitalia <vita.kshevitska@gmail.com>
2024-12-03 12:05:35 +0100

    chore: add storyblok and extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./storyblok/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./hooks/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     fontFamily: {
