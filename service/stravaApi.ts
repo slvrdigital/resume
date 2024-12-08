26d45c871ea7308f15675ee8d26fcd1871171244
Vitalia <vita.kshevitska@gmail.com>
2024-12-08 22:37:15 +0100

    fix: data flat in fetchExtendedActivities method

--- service/stravaApi.ts
+++ service/stravaApi.ts
@@ -78,7 +78,7 @@ export async function fetchExtendedActivities() {
     const { data } = await fetchActivities();
     const list = await Promise.all(data.map((i) => fetchActivity(i.id)));
 
-    return { data: list.map((i) => i.data) };
+    return { data: list.map((i) => i.data).flat() };
   } catch (error) {
     console.error(error);
     throw new Error("Failed to fetch activities");
