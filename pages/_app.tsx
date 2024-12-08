80c7985cb38005998e07844c4007b21b1c0b9cc2
Vitalia <vita.kshevitska@gmail.com>
2024-12-08 22:36:37 +0100

    chore: remove useless code from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
-import "@/styles/globals.css";
-
 import Layout from "@/components/Layout";
 import Meta from "@/components/Meta";
-import { Inter } from "next/font/google";
 import type { AppProps } from "next/app";
-
-const inter = Inter({ subsets: ["latin"], weight: ["400"] });
+import "@/styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const { meta } = pageProps;
