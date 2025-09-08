7cab290cb20d394303107ddd7cc9af0055504784
Yevhenii Kshevitskyi <yevhenii@eli5.io>
2025-09-08 13:32:46 +0200

    chore: add goatcounter to csp

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const cspHeader = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
-  img-src 'self' blob: data:;
+  img-src 'self' blob: data: https://yevheni.goatcounter.com;
   font-src 'self';
   object-src 'none';
   base-uri 'self';
